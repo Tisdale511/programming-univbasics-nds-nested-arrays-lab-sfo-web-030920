617b019d0feb79959cf81b3405e4379b9effa6ad
Tisdale Fry <tisdalefry@gmail.com>
2020-02-25 01:00:26 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -38,7 +38,7 @@ end
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
-  p sorted_matrix[1][1]
+    sorted_matrix[1][1]
   p assembled_matrix[1][1]
 end
 
